a2cab18f4280c054b380bf6e8b0f6e6ff399e4ad
basv1996 <bas.vugts@gmail.com>
2021-12-02 15:53:07 +0100

    added wiki img

--- src/hooks/useD3.js
+++ src/hooks/useD3.js
@@ -3,6 +3,12 @@ import * as d3 from "d3";
 
 const useD3 = (renderChartFn) => {
   const ref = React.useRef();
+  //UseRef
+  //Een ref lijkt op een state, 
+  //maar ref laat je component niet opnieuw update als je component rendert zoals je dat wel bij state hebt.
+  //een ref returnt maar 1 value
+
+
 
   React.useEffect(() => {
     renderChartFn(d3.select(ref.current));
